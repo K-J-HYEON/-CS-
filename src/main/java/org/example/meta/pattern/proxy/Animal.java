c774e83c8d9811e5a497f131f2c2a2236616e449
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2023-01-06 08:27:50 +0900

    프록시 패턴 예시

---
+++ src/main/java/org/example/meta/pattern/proxy/Animal.java
@@ -0,0 +1,8 @@
+package org.example.meta.pattern.proxy;
+
+
+// 추상적인 타겟으로 옮겨 바라보는 것
+public abstract class Animal {
+    public abstract String getName();
+
+}
---
+++ src/main/java/org/example/meta/pattern/proxy/App.java
@@ -0,0 +1,11 @@
+package org.example.meta.pattern.proxy;
+
+public class App {
+    public static void main(String[] args) {
+        Mouse m = new Mouse();
+        Cat c = new Cat();
+        DoorMan dm = new DoorMan();
+        dm.쫒아내(m);
+        dm.쫒아내(c);
+    }
+}
